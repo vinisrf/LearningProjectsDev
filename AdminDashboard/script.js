34354ddcddf6446d0421465828cc0287384421e6
vinisrf <vinisrf@live.com>
2024-05-10 11:41:16 -0300

    up_100524

---
+++ AdminDashboard/script.js
@@ -0,0 +1,9 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+    const li = item.parentElement;
+
+    item.addEventListener('click', function () {
+        li.classList.add('active');
+    })
+});
