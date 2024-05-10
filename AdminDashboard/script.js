c154d160d784f87c551ae5fd2479195f207290a1
vinisrf <vinisrf@live.com>
2024-05-10 12:48:32 -0300

    up_200524

--- AdminDashboard/script.js
+++ AdminDashboard/script.js
@@ -1,9 +1,13 @@
+/* destacar visualmente o item do menu que está atualmente selecionado. */
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
 allSideMenu.forEach(item=> {
     const li = item.parentElement;
 
     item.addEventListener('click', function () {
+        allSideMenu.forEach(i=> {
+            i.parentElement.classList.remove('active');
+        })
         li.classList.add('active');
     })
 });
