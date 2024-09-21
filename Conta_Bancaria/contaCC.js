9d2198bce22f1aeb178e796225255a2a9da53d8a
vinisrf <vinisrf@live.com>
2024-09-21 17:55:18 -0300

    up_210924

--- Conta_Bancaria/contaCC.js
+++ Conta_Bancaria/contaCC.js
@@ -1,9 +1,12 @@
 const input = require ('prompt-sync')();
 
-/** Conta Bancaria 
+/**
+ * Conta Bancaria 
  * 
- * Em desenvolvimento!
- */
+ * desenvolvimento
+ * 
+ * */  
+
 
 
 const contaCC = {
@@ -17,12 +20,22 @@ const contaCC = {
 
 /** Variaveis globais */
 
-const entrada = parseInt(input("PARA ABRI CONTA DIGITE [1] ou [2] PARA CANCELAR:"));
-let abreConta = entrada;
+let abreConta = parseInt(input("PARA ABRIR CONTA TECLE [1] PARA CANCELAR TECLE [2]:"));
+let numConta = [null,null,null,null,".",null,null,null,null,".",null,null,null,null,".",null,null,null,null];
 
 /**--------------------- */
 
 /** Funções */
+
+//função: Inicio
+function inicio(){
+        
+    
+        criaUser();
+        aprovaCredito();
+        confirmaDados();
+        
+}
 
 // função: criar usuário
 function criaUser (){
@@ -31,7 +44,7 @@ function criaUser (){
     contaCC.Usuario = nomeUser;
     
     return contaCC.Usuario;
- }
+}
 
 // função: para aprovar crédito
 function aprovaCredito (){
@@ -41,45 +54,81 @@ function aprovaCredito (){
     return contaCC.Credito;
     
 }
+
 // função: Após informar nome e renda mensal, o sistema pesrgunta se deseja confirmar e prosseguir ou se deseja editar os dados.
 function confirmaDados(){
-    let confDados = parseInt (input("PARA CONFIRMAR OS DADOS TECLE [1] PARA MUDAR OS DADO TECLE [2]: "));
+    let confDados = parseInt (input("PARA CONFIRMAR OS DADOS TECLE [1] PARA MUDAR OS DADOS TECLE [2]: "));
     
     if(confDados == 1){
-        console.log("CONTA CRIADA! ", "USUARIO: "  + contaCC.Usuario+ " CREDITO DISPONIVEL: " + contaCC.Credito.toFixed(3))
-        
+        geraNumConta()
+        console.log("------------------CONTA CRIADA!--------------------");
+        console.log("NÚMEOR DA CONTA: "+ contaCC.NumeroConta.join(' '));
+        console.log("USUARIO: "  + contaCC.Usuario);
+        console.log("CREDITO DISPONIVEL: " + contaCC.Credito.toFixed(3));
+        console.log("--------------------------------------------------");
 
     }else if(confDados == 2){
         criaUser();
         aprovaCredito();
         confirmaDados();
+    }else{
+        return confirmaDados();
     }
 }
 
 // Função: Gerar numero da conta
+function geraNumConta (){
+    let min = 0;
+    let max = 9;
 
+    numConta = numConta.map( value => digitosConta = Math.floor(Math.random() * (max - min + 1)) + min);
+        numConta.splice(4,1,'.')
+        numConta.splice(9,1,'.')
+        numConta.splice(14,1,'.');
 
-// Função: Criar nova conta
+        contaCC.NumeroConta = numConta;
+
+        return contaCC.NumeroConta
+}
+
+// função: volta para o inicio
+function retornoInicio(){
+    
+    let abreConta = parseInt(input("PARA ABRIR CONTA TECLE [1] PARA CANCELAR TECLE [2]:"));
+    while (abreConta != 1) {
+        console.log("OPÇÃO INVÁLIDA!");
+        retornoInicio();
+        break;
+    }if(abreConta == 1){
+        
+        inicio();
+        
+    }
+        
+}
+// função: Criar senha
+
+// função: Criar nova conta
 
 /**------------------------- */
 
+// switch: inicio do sistema
 switch (abreConta){
     
     case 1:
         
-        criaUser();
-        aprovaCredito();
-        confirmaDados();
+        inicio();
 
 
         break;
 
     case 2:
-        console.log("Operação cancelada")
+        console.log("OOPERAÇÃO CANCELADA!");
         
         break;
 
     default:
-        console.log("OPÇÃO INCORRETA!")
+        console.log("OPÇÃO INVÁLIDA!");
+        retornoInicio();
      
 }
